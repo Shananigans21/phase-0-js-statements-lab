f1ac6e8230039d2beae7f1e9768e1fdbe513f3d9
Shana Lynch <shana.lynch21@gmail.com>
2025-04-12 19:28:10 -0400

    Implement garden management logic using statements and loops

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,31 @@
+// Provided variables
 const temperature = 90;
 const timeOfDay = "morning";
 let soilMoisture = 30;
+
+// 1. Check the temperature
+if (temperature > 80) {
+  console.log("Watering on");
+} else {
+  console.log("Watering off");
+}
+
+// 2. Check the time of day
+if (timeOfDay === "evening" || timeOfDay === "night") {
+  console.log("Lights on");
+} else {
+  console.log("Lights off");
+}
+
+// 3. Increase soilMoisture to 40
+while (soilMoisture < 40) {
+  soilMoisture += 5;
+  console.log(soilMoisture);
+}
+
+// Needed for testing
+module.exports = {
+  temperature,
+  timeOfDay,
+  soilMoisture
+};
